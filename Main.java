9ac47589de0168bdc873df2914835039127a57b4
AnaaLuizaa-a <164022945+AnaaLuizaa-a@users.noreply.github.com>
2024-03-22 08:21:06 -0300

    Add files via upload
    Questão 1

---
+++ Main.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    Scanner teclado = new Scanner( System.in );
+    // Declaracao de variaveis
+    int nota=0;
+    System.out.println("Digite uma nota: ");
+    nota = teclado.nextInt();
+    while (nota > 10 || nota < 0){
+
+      System.out.println("Nota invalida.Digite novamente");
+
+      System.out.println("Digite uma nota: ");
+      nota = teclado.nextInt();
+    }
+     System.out.println("Nota valida");
+   }
+
+
+}
