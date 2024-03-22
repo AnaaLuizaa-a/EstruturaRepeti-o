81e5a605f0967f4503d7538660036a2a48413364
AnaaLuizaa-a <164022945+AnaaLuizaa-a@users.noreply.github.com>
2024-03-22 08:35:46 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,17 +2,20 @@
 import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    //entrada de nome e senha
-    String nome, senha;
-      System.out.println("Digite o nome de usuário: ");
-     nome = input.next();
-      System.out.println("Digite a senha: ");
-     senha = input.next();
-    //validação de senha
-    while (nome.equals(senha)){
-      System.out.println("Senha invalida. Digite novamente:");
-     senha = input.next(); }
-    System.out.println("Senha valida. Bem vindo!");
-  }
+    Scanner teclado = new Scanner( System.in );
+    // Declaracao de variaveis
+    int nota=0;
+    System.out.println("Digite uma nota: ");
+    nota = teclado.nextInt();
+    while (nota > 10 || nota < 0){
+
+      System.out.println("Nota invalida.Digite novamente");
+
+      System.out.println("Digite uma nota: ");
+      nota = teclado.nextInt();
+    }
+     System.out.println("Nota valida");
+   }
+
+
 }
